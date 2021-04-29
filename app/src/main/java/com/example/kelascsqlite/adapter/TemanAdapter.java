119124038493d8b079ed3445912c3e3037b57d72
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2021-04-29 13:39:39 +0700

    First push

---
+++ app/src/main/java/com/example/kelascsqlite/adapter/TemanAdapter.java
@@ -0,0 +1,60 @@
+package com.example.kelascsqlite.adapter;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kelascsqlite.R;
+import com.example.kelascsqlite.database.Teman;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+        String nm, tlp;
+
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+        holder.namaTxt.setText(nm);
+        holder.namaTxt.setTextColor(Color.BLUE);
+        holder.namaTxt.setTextSize(20);
+        holder.telponTxt.setText(tlp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData != null ? listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+        public TemanViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardku = itemView.findViewById(R.id.cardKu);
+            namaTxt = itemView.findViewById(R.id.textNama);
+            telponTxt = itemView.findViewById(R.id.textTelpon);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kelascsqlite/database/DBController.java
@@ -0,0 +1,56 @@
+package com.example.kelascsqlite.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DBController extends SQLiteOpenHelper {
+    public DBController(Context context) {
+        super(context, "ProdiTI", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table teman (id integer primary key, nama text, telpon text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists teman");
+        onCreate(db);
+    }
+
+    public void insertData(HashMap<String, String> queryValues){
+        SQLiteDatabase basisData = this.getWritableDatabase();
+        ContentValues nilai = new ContentValues();
+        nilai.put("nama",queryValues.get("nama"));
+        nilai.put("telpon",queryValues.get("telpon"));
+        basisData.insert("teman", null, nilai);
+        basisData.close();
+    }
+
+    public ArrayList<HashMap<String, String>> getAllTeman(){
+        ArrayList<HashMap<String, String>> daftarTeman = new ArrayList<HashMap<String, String>>();
+
+        String query = "SELECT * FROM teman";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.moveToFirst()){
+            do{
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("id",cursor.getString(0));
+                map.put("nama",cursor.getString(1));
+                map.put("telpon",cursor.getString(2));
+                daftarTeman.add(map);
+            }while(cursor.moveToNext());
+        }
+        return daftarTeman;
+    }
+}
